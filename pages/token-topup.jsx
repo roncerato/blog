6b4d336a8e6854af518234b6caa3ea06211c9e95
roncerato <nazarmuhamedow9731@gmail.com>
2024-09-21 23:10:03 +0500

    Add custom layout functionality for TokenTopup page using AppLayout

--- pages/token-topup.jsx
+++ pages/token-topup.jsx
@@ -1,5 +1,5 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-
+import { AppLayout } from "../components/AppLayout";
 export default function TokenTopup() {
     return (
       <div>
@@ -9,6 +9,12 @@ export default function TokenTopup() {
       </div>
     );
   }
+
+  TokenTopup.getLayout = function getLayout(page, pageProps){
+    return <AppLayout {...pageProps}>{page}</AppLayout>
+  }
+  
+
   export const getServerSideProps = withPageAuthRequired(() => {
     return {
       props: {}
