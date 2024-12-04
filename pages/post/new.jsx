659d41b9320b25777c1d6ac64f1808e24493bccc
ruslannazari <nazarmuhamedowruslan@gmail.com>
2024-12-04 17:26:58 +0500

    Added a button and a click handler for debugging the generatePost function

--- pages/post/new.jsx
+++ pages/post/new.jsx
@@ -2,16 +2,28 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
 
 export default function NewPost(props) {
+
+  const handleClick = async () => {
+    const response = await fetch(`/api/generatePost`, {
+      method: "POST"
+    })
+    const json = await response.json()
+    console.log("RESULT: ", json)
+  }
+
   return (
     <div>
       <h1>
         This is the new post page
       </h1>
+      <button className="btn" onClick={handleClick}>
+        Generate
+      </button>
     </div>
   );
 }
 
-NewPost.getLayout = function getLayout(page, pageProps){
+NewPost.getLayout = function getLayout(page, pageProps) {
   return <AppLayout {...pageProps}>{page}</AppLayout>
 }
 
